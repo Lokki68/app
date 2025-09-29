d8c9bd87eb3a018824aabe823478dbac70c5a441
Libert Brice <b.libert@ideematic.com>
2025-09-29 18:03:14 +0200

    Fix connect to db

--- lib/utils/db/connectToDb.ts
+++ lib/utils/db/connectToDb.ts
@@ -2,15 +2,14 @@ import mongoose from "mongoose";
 
 export async function connectToDb() {
   if (mongoose.connection.readyState) {
-    console.log("Using existing connection :", mongoose.connection.name)
+    console.log("Using existing connection :", mongoose.connection.name);
   }
 
   try {
-    const mongoConnectUrl = process.env.MONGO || ''
-    await mongoose.connect(mongoConnectUrl)
-    console.log("Connected to database : ", mongoose.connection.name)
+    await mongoose.connect(process.env.MONGO as string);
+    console.log("Connected to database : ", mongoose.connection.name);
   } catch (error) {
-    throw new Error("Failed to connect to database")
-
+    const errorMessage = error instanceof Error ? error.message : String(error);
+    throw new Error("Failed to connect to database: " + errorMessage);
   }
 }
