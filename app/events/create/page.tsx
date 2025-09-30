8bb53a3c4cd6613e5d6053f4cb5143d54d14c799
Libert Brice <b.libert@ideematic.com>
2025-09-30 09:13:40 +0200

    form event

--- app/events/create/page.tsx
+++ app/events/create/page.tsx
@@ -1,13 +1,14 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 const page = () => {
   return (
     <main>
       <Card>
         <CardHeader>
-          <h1>Créer un nouvel évenement</h1>
+          <h1 className="text-center text-xl">Créer un nouvel évenement</h1>
         </CardHeader>
         <CardContent>
           <form className="grid gap-4">
@@ -15,6 +16,19 @@ const page = () => {
               <Label htmlFor="title">Titre</Label>
               <Input id="title" name="title" />
             </div>
+            <div className="flex flex-col gap-2">
+              <Label>Date</Label>
+              <Input id="date" name="date" type="date" />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="description">Description</Label>
+              <Textarea
+                id="description"
+                name="description"
+                placeholder="Description de l'évenement"
+              />
+            </div>
+            <div className="flex flex-col gap-2"></div>
           </form>
         </CardContent>
       </Card>
